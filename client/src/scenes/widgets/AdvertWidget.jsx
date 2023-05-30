30a669fa91e5893305d1f81d018c72a046ac0ceb
kaiwalya-07 <kaiwalyadudhgaonkar3@gmail.com>
2023-05-30 21:46:01 +0530

    first commit

---
+++ client/src/scenes/widgets/AdvertWidget.jsx
@@ -0,0 +1,37 @@
+import { Typography, useTheme } from "@mui/material";
+import FlexBetween from "components/FlexBetween";
+import WidgetWrapper from "components/WidgetWrapper";
+
+const AdvertWidget = () => {
+  const { palette } = useTheme();
+  const dark = palette.neutral.dark;
+  const main = palette.neutral.main;
+  const medium = palette.neutral.medium;
+
+  return (
+    <WidgetWrapper>
+      <FlexBetween>
+        <Typography color={dark} variant="h5" fontWeight="500">
+          Sponsored
+        </Typography>
+        <Typography color={medium}>Try once</Typography>
+      </FlexBetween>
+      <img
+        width="100%"
+        height="auto"
+        alt="advert"
+        src="http://localhost:3001/assets/info4.jpeg"
+        style={{ borderRadius: "0.75rem", margin: "0.75rem 0" }}
+      />
+      <FlexBetween>
+        <Typography color={main}>BRO FOODS</Typography>
+        <Typography color={medium}>brofoods.com</Typography>
+      </FlexBetween>
+      <Typography color={medium} m="0.5rem 0">
+      The best in town to serve your taste buds .
+      </Typography>
+    </WidgetWrapper>
+  );
+};
+
+export default AdvertWidget;
