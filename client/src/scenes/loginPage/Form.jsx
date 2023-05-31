1405f03f72614a4c0c78d753e866787b1d92fb43
kaiwalya-07 <kaiwalyadudhgaonkar3@gmail.com>
2023-05-31 18:48:13 +0530

    Register bug fix

--- client/src/scenes/loginPage/Form.jsx
+++ client/src/scenes/loginPage/Form.jsx
@@ -64,7 +64,7 @@ const Form = () => {
     formData.append("picturePath", values.picture.name);
 
     const savedUserResponse = await fetch(
-      "https://social-n1sl.onrender.comauth/register",
+      "https://social-n1sl.onrender.com/auth/register",
       {
         method: "POST",
         body: formData,
@@ -262,7 +262,7 @@ const Form = () => {
               }}
             >
               {isLogin
-                ? "Don't have an account? Sign Up here."
+                ? "New here? Sign Up ."
                 : "Already have an account? Login here."}
             </Typography>
           </Box>
